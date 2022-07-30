ab7cebbeeacfcc8618874b31ece7e2f8a3c65746
Felix Titov <titow.feliks@yandex.ru>
2022-07-30 23:30:32 +0300

    Created all functionality

--- CoreDataApp/TaskListViewController.swift
+++ CoreDataApp/TaskListViewController.swift
@@ -8,15 +8,22 @@
 
 
 import UIKit
+import CoreData
 
 class TaskListViewController: UITableViewController {
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let cellID = "task"
+    private var taskList = [Task]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         setupNavBar()
+        fetchData()
     }
     
     private func setupNavBar() {
@@ -29,11 +36,85 @@ class TaskListViewController: UITableViewController {
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
         navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addNewTask))
         
+        navigationController?.navigationBar.tintColor = .white
         navigationController?.navigationBar.standardAppearance = navBarAppearance
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
     }
+    
+    private func showAlert(with title: String, and message: String) {
+        let alertConteroller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            guard let task = alertConteroller.textFields?.first?.text, !task.isEmpty else { return }
+            self.save(task)
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
+        
+        alertConteroller.addAction(saveAction)
+        alertConteroller.addAction(cancelAction)
+        alertConteroller.addTextField { textField in
+            textField.placeholder = "New task"
+        }
+        
+        present(alertConteroller, animated: true)
+    }
+    
+    private func fetchData() {
+        let request = Task.fetchRequest()
+        
+        do {
+            taskList = try context.fetch(request)
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    private func save(_ taskName: String) {
+        guard let entityDescription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
+        
+        guard let task = NSManagedObject(entity: entityDescription, insertInto: context) as? Task else { return }
+        
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch let error {
+                print(error)
+            }
+        }
+    }
 
+    @objc private func addNewTask() {
+        showAlert(with: "New task", and: "Add new task")
+    }
+    
+}
 
+//MARK: - UITableViewDataSource
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        let task = taskList[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
 }
 
